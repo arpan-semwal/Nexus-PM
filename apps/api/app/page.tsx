9bfffd00807d619a2763b5ad39814aeeae3a4495
arpan-semwal <arpansemwal8@gmail.com>
2026-02-04 21:43:49 +0530

    changes in package.json

--- apps/api/app/page.tsx
+++ apps/api/app/page.tsx
@@ -12,8 +12,8 @@ const ThemeImage = (props: Props) => {
 
   return (
     <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
+      <Image alt="Light theme image" {...rest} src={srcLight} className="imgLight" />
+      <Image alt="Dark theme image" {...rest} src={srcDark} className="imgDark" />
     </>
   );
 };
